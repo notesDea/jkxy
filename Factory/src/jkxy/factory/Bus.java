5ef88817a57aff9444007b0490bde0759c7f2f17
notesDea <notesdea@outlook.com>
2016-07-08 15:21:20 +0800

    optimized code

---
+++ Factory/src/jkxy/factory/Bus.java
@@ -0,0 +1,11 @@
+package jkxy.factory;
+
+/**
+ * Created by dea on 16-7-8.
+ */
+public class Bus extends Vehicle {
+    @Override
+    public String run() {
+        return "Bus is running";
+    }
+}
